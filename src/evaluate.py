d815842a10d86face75f660731945ac01b377b3d
Anurag Mahalpure <anuragmahalpure@gmail.com>
2025-05-11 14:52:58 +0530

    Day 1 - 8

--- src/evaluate.py
+++ src/evaluate.py
@@ -65,20 +65,20 @@ def m_evaluate(config_file):
     batch = config['img_augment']['batch_size']
     class_mode = config['img_augment']['class_mode']
     te_set = config['model']['test_path']
-    model = load_model('mlops_project/models/trained.h5')
+    model = load_model('models/trained.h5')
     config = get_data(config_file)
 
     test_gen = ImageDataGenerator(rescale = 1./255)
     test_set = test_gen.flow_from_directory(te_set,
-                                                target_size = (225,225),
+                                                target_size = (255, 255),
                                                 batch_size = batch,
                                                 class_mode = class_mode
                                                 )
     label_map=(test_set.class_indices)
     print(label_map)
 
-    Y_pred = model.predict(test_set, len(test_set))
-    y_pred = np.argmax(Y_pred, axis=1)
+    y_pred = model.predict(test_set, len(test_set))
+    y_pred = np.argmax(y_pred, axis=1)
     print("Confusion Matrix")
     sns.heatmap(confusion_matrix(test_set.classes,y_pred),annot=True)
     plt.xlabel('Actual vlaues,0:Bulbasaur, 1:Charmander, 2: Squirtle,3:Tauros')
